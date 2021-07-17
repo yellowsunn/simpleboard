33bbf0614804a09e8991f13de555715e38376fe1
yellowsunn <chohankook95@gmail.com>
2021-07-17 20:50:34 +0900

    init

---
+++ src/main/java/com/yellowsunn/simpleforum/SimpleForumApplication.java
@@ -0,0 +1,13 @@
+package com.yellowsunn.simpleforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yellowsunn/simpleforum/SimpleForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yellowsunn.simpleforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
