9f9c507565081a5a43d27afe003bff42925cecef
yellowsunn <chohankook95@gmail.com>
2021-07-30 17:56:21 +0900

    lucy xss filter 적용

--- src/main/java/com/yellowsunn/simpleforum/config/ApiConfig.java
+++ src/main/java/com/yellowsunn/simpleforum/config/ApiConfig.java
@@ -1,8 +1,11 @@
 package com.yellowsunn.simpleforum.config;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import com.yellowsunn.simpleforum.api.argumentresolver.LoginIdArgumentResolver;
 import com.yellowsunn.simpleforum.api.interceptor.AdminCheckInterceptor;
 import com.yellowsunn.simpleforum.api.interceptor.LoginCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -28,5 +31,15 @@ public class ApiConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginIdArgumentResolver());
+    }
+
+    //Lucy Xss filter 적용
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+        FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 }
