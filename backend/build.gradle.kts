03abe12c54a450acb1f520553219146ddb75707f
yellowsunn <chohankook95@gmail.com>
2023-05-02 13:54:20 +0900

    java version up 11 -> 17

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.yellowsunn"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
     compileOnly {
