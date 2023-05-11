304d1bccb96444f401bf9abbffd79d473b2c6dc5
yellowsunn <chohankook95@gmail.com>
2023-05-11 19:05:55 +0900

    redis session으로 변경

--- user-service/user-api/build.gradle.kts
+++ user-service/user-api/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.session:spring-session-data-redis")
 
     implementation(project(":user-service:user-core"))
     runtimeOnly(project(":user-service:infra-persistence"))
