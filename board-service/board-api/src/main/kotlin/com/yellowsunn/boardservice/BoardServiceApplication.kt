06ae4ca6b631261b8c079bc27bbb4e4baf292261
yellowsunn <chohankook95@gmail.com>
2023-05-19 11:03:05 +0900

    board-service 초기화

---
+++ board-service/board-api/src/main/kotlin/com/yellowsunn/boardservice/BoardServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.yellowsunn.boardservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardServiceApplication>(*args)
+}
---
+++ board-service/build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version BoardServiceVersions.springBoot
+    id("io.spring.dependency-management") version BoardServiceVersions.springDependencyManagement
+    kotlin("jvm") version BoardServiceVersions.kotlin
+    kotlin("plugin.spring") version BoardServiceVersions.kotlin
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+subprojects {
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+}
