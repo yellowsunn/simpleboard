ca407093ebbdaf43ee9831e6b246babed9dac4a7
yellowsunn <chohankook95@gmail.com>
2023-06-09 19:49:02 +0900

    jib 추가

--- board-service/apps/board-api/build.gradle.kts
+++ board-service/apps/board-api/build.gradle.kts
@@ -1,3 +1,9 @@
+import com.google.cloud.tools.jib.gradle.JibExtension
+
+plugins {
+    id("com.google.cloud.tools.jib") version BoardServiceVersions.jib
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -10,4 +16,13 @@ dependencies {
     runtimeOnly(project(":board-service:libs:infra-kafka-producer"))
     runtimeOnly(project(":board-service:libs:infra-mongodb"))
     runtimeOnly(project(":board-service:libs:infra-redis"))
+}
+
+configure<JibExtension> {
+    from {
+        image = "eclipse-temurin:17-jre-alpine"
+    }
+    container {
+        ports = listOf("8080")
+    }
 }
