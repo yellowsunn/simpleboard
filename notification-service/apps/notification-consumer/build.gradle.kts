948e0e83db16af376bc99cc39c40e3b8a607d8d3
yellowsunn <43487002+yellowsunn@users.noreply.github.com>
2024-03-10 14:25:42 +0900

    notification dev 인프라 수정 (#36)
    * notification dev 인프라 수정
    * notification consumer 인프라 수정

--- notification-service/apps/notification-consumer/build.gradle.kts
+++ notification-service/apps/notification-consumer/build.gradle.kts
@@ -4,15 +4,24 @@ plugins {
     id("com.google.cloud.tools.jib") version NotificationVersions.jib
 }
 
+extra["springCloudVersion"] = "2022.0.4"
+
 dependencies {
     implementation(project(":common-library"))
     implementation(project(":notification-service:libs:notification-core"))
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     runtimeOnly(project(":notification-service:libs:infra-mongodb"))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 configure<JibExtension> {
